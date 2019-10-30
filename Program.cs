ad0f67a78821ff2a8a1a9f6d4ea43e1f5cad7906
Ishmael Heathcliff <darkflare1055@gmail.com>
2019-10-30 13:00:40 +0800

    init repository

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AutoSummary
+{
+    class Program
+    {
+        static StringBuilder _summary = new StringBuilder();
+        static bool _isRoot = true;
+        static String _tab = "    ";
+        static int _tabCount = 0;
+
+        public static void AutoSummary(string dir)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+
+
+            if (_isRoot)
+            {
+                _summary.Append("# SUMMARY\n");
+                _isRoot = false;
+            }
+            
+            FileSystemInfo[] subDirsAndFilesInfo = dirInfo.GetFileSystemInfos();
+
+            foreach (FileSystemInfo fsInfo in subDirsAndFilesInfo)
+            {
+                if (fsInfo is DirectoryInfo)
+                {
+                    if (fsInfo.Name == "assets") continue;
+
+                    for (int i = 0; i < _tabCount; i++) _summary.Append(_tab);
+                    _summary.Append($"- {fsInfo.Name}\n");
+                    _tabCount += 1;
+                    AutoSummary(fsInfo.FullName);
+                }
+                else if(fsInfo.Name.EndsWith(".md"))
+                {
+                    if (fsInfo.Name == "SUMMARY.md") continue;
+                        for (int i = 0; i < _tabCount; i++) _summary.Append(_tab);
+                    _summary.Append(
+                        $"- [{fsInfo.Name.Split(new[]{".md"}, StringSplitOptions.RemoveEmptyEntries)[0]}]({dir}\\{fsInfo.Name})\n"
+                        );
+                }
+            }
+
+            _tabCount -= 1;
+        }
+
+        public static void Main(string[] args)
+        {
+            AutoSummary(".");
+            var writer = new StreamWriter("SUMMARY.md");
+            using (writer)
+            {
+                writer.Write(_summary.ToString());
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+## Introduce
+
+此工具可自动创建目录下markdown文件的结构和引用
+
+将AutoSummary.exe放在目标文件夹中并双击运行
+
+结果将保存在SUMMARY.md中
+
+## Attention
+
+自动忽略assets文件夹
+
+其他无关文件夹不会忽略
+
